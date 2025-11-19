73dbe9d41beee9284f865e418b8f4e8c9239e264
ItsSilva <juanse909@hotmail.com>
2025-11-19 14:49:23 -0500

    slice add

---
+++ my-react-app/src/redux/slices/GetCharactersSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+interface Characters {
+  id: number;
+  status: string;
+  species: string;
+}
+
+interface CharactersState {
+  characters: Characters[];
+}
+
+const initialState: CharactersState = {
+  characters: [],
+};
+
+export const GetCharactersSlice = createSlice({
+  name: "getCharacters",
+  initialState,
+  reducers: {
+    getCharacters: (state, action: PayloadAction<Characters[]>) => {
+      state.characters = action.payload;
+    },
+    addCharacter: (state, action: PayloadAction<Characters>) => {
+      state.characters = [...state.characters, action.payload];
+    },
+    deleteCharacter: (state, action: PayloadAction<number>) => {
+      state.characters = state.characters.filter(
+        (character) => character.id !== action.payload
+      );
+    },
+    editCharacter: (state, action: PayloadAction<Characters>) => {
+      const index = state.characters.findIndex(
+        (character) => character.id === action.payload.id
+      );
+      if (index !== -1) {
+        state.characters[index] = action.payload;
+      }
+    },
+  },
+});
+
+export const { getCharacters, addCharacter, deleteCharacter, editCharacter } = GetCharactersSlice.actions;
+export default GetCharactersSlice.reducer;
