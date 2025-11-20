e3037e0ddf60c309b2157bb824eb3bfdbacbeaa1
ItsSilva <juanse909@hotmail.com>
2025-11-19 19:10:20 -0500

    add form add

--- my-react-app/src/pages/Add.tsx
+++ my-react-app/src/pages/Add.tsx
@@ -1,4 +1,58 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { addCharacter } from "../redux/slices/GetCharactersSlice";
+
 const Add = () => {
-    return <div>Add Page</div>;
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [name, setName] = useState('');
+    const [status, setStatus] = useState('');
+    const [species, setSpecies] = useState('');
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (name.trim() && status.trim() && species.trim()) {
+            const formInfo = {
+                id: Date.now(),
+                name: name.trim(),
+                status: status.trim(),
+                species: species.trim()
+            }
+            dispatch(addCharacter(formInfo));
+            setName('');
+            setStatus('');
+            setSpecies('');
+            navigate('/');
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Name:</label>
+                <input type="text" 
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+                        <div>
+                <label>Status:</label>
+                <input type="text" 
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+                />
+            </div>
+                        <div>
+                <label>Species:</label>
+                <input type="text" 
+                value={species}
+                onChange={(e) => setSpecies(e.target.value)}
+                />
+            </div>
+            <button type="submit">Add new character</button>
+        </form>
+    )
 }
 export default Add;
